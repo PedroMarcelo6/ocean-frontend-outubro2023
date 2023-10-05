f729fa74441cccf1de249777b11612ac6cd62f48
PedroMarcelo6 <47678306+PedroMarcelo6@users.noreply.github.com>
2023-10-05 13:57:11 +0000

    Novas mudanças

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,29 @@
 import './App.css'
+import Card from './components/Card/Card'
+
+/*
+- Layout:
+- Conteúdo está repetido/não está dinâmico
+*/
 
 function App() {
+  const personagem1 = {
+    nome: 'Rick Sanchez',
+    imagemUrl: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg'
+  }
+
+  const personagem2 = {
+    nome: 'Morty Smith',
+    imagemUrl: 'https://rickandmortyapi.com/api/character/avatar/2.jpeg'
+  }
+
   return (
     <>
-      <div className='card'>
-        <h2> Rick Sanchez</h2>
-        <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" />
-      </div>
+      <Card item={personagem1} />
+      <Card item={personagem2} />
     </>
   )
 }
 
 export default App
-
 
